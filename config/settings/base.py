803bf2c8f3480d956ef7716b7dd6cbae1332e71c
unknown <anandkassu00@gmail.com>
2019-06-24 19:08:47 +0530

    changed base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,5 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
