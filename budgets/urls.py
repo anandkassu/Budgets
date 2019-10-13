12cc096b5aa04e9341356b1ad99f501efd10bff8
anandkassu <anandkassu00@gmai.com>
2019-10-13 21:53:07 +0530

    ulr changes

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+from django.conf.urls import url
+
 from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', views.home),
-    path('contact', views.contact),
-    path('add_new', views.AddItemView.as_view()),
-    path('view', views.ListItemView.as_view()),
+    url(r'^$', views.home),
+    url(r'^contact', views.contact),
+    url(r'^add_new', views.AddItemView.as_view()),
+    url(r'^views', views.ListItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/$', views.DetailItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/edit/$', views.UpdateItemView.as_view()),
     url(r'^view/(?P<pk>\w+)/delete/$', views.DeleteItemView.as_view()),
